97ef1e8f7916373b014307c924beb0af9ebbb2b8
Guangxin Yu <1572250276@qq.com>
2022-12-09 14:48:53 +0800

    merge code

--- pages/shows/management/index.js
+++ pages/shows/management/index.js
@@ -1,4 +1,9 @@
 // pages/shows/management/index.js
+import event from '@codesmiths/event';
+import { checkShowExpired } from '../../../utils/util';
+
+const globalData = getApp().globalData;
+
 Page({
 
 	/**
@@ -6,6 +11,59 @@ Page({
 	 */
 	data: {
 
+	},
+
+	/**
+	 * navigate to show new page
+	 */
+	onNavigateToShowNew() {
+		wx.navigateTo({
+			url: '/pages/shows/new/index'
+		})
+	},
+
+	/**
+	 * navigate to detail page (upcoming show)
+	 */
+	onNavigateToDetailUpcoming(e) {
+		// 1. get id of show
+		const { id } = e.currentTarget.dataset;
+
+		// 2. navigate to detail page
+		wx.navigateTo({
+			url: `/pages/shows/show/index?id=${id}&isExpired=false`,
+		})
+	},
+
+	/**
+	 * navigate to detail page (expired show)
+	 */
+	onNavigateToDetailExpired(e) {
+		// 1. get id of show
+		const { id } = e.currentTarget.dataset;
+
+		// 2. navigate to detail page
+		wx.navigateTo({
+			url: `/pages/shows/show/index?id=${id}&isExpired=true`,
+		})
+	},
+
+	/**
+	 * fetch created shows
+	 */
+	onFetchCreatedShows() {
+		// 1. find user id
+		const _this = this;
+		const { id } = globalData.user;
+
+		// 2. fetch shows created by this user
+		wx.request({
+			url: `${globalData.baseUrl}/users/${id}/created_shows`,
+			header: globalData.header,
+			success(res) {
+				checkShowExpired(_this, res.data.shows);
+			}
+		})
 	},
 
 	/**
@@ -26,7 +84,12 @@ Page({
 	 * Lifecycle function--Called when page show
 	 */
 	onShow() {
-
+		// get all shows created by current user
+		if (globalData.header) {
+			this.onFetchCreatedShows()
+		} else {
+			event.on('tokenReady', this, this.onFetchCreatedShows)
+		}
 	},
 
 	/**
